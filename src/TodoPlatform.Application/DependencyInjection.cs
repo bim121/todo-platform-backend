86f116c1b7b2952d5d5b3218f9c7bd208bb2a1d1
Vitaliy Tishik <vitisik54@gmail.com>
2026-06-10 23:02:08 +0300

    add mediatr and fluent

--- src/TodoPlatform.Application/DependencyInjection.cs
+++ src/TodoPlatform.Application/DependencyInjection.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 using TodoPlatform.Application.Services;
 
@@ -7,6 +8,11 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddMediatR(cfg =>
+            cfg.RegisterServicesFromAssembly(typeof(ApplicationAssemblyMarker).Assembly));
+
+        services.AddValidatorsFromAssembly(typeof(ApplicationAssemblyMarker).Assembly);
+
         services.AddScoped<ITodoService, TodoService>();
         services.AddScoped<IAuthService, AuthService>();
         return services;
